543d13e62932e9d8e56f44055e1e23f0aa2adb39
Your Name <107790440+daikingkong@users.noreply.github.com>
2022-08-16 07:22:28 +0000

    [Add]ルーティングの追加

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,34 @@ Rails.application.routes.draw do
     sessions: "admin/sessions"
   }
 
+  namespace :admin do
+    get '' => 'homes#top'
+    resources :items, except: [:destroy]
+    resources :genres, only: [:index, :create, :edit, :update]
+    resources :customers, only: [:index, :show, :edit, :update]
+    resources :orders, only: [:show, :update]
+    resources :order_details, only: [:update]
+  end
+
+  scope module: :public do
+    root 'homes#top'
+    get 'about' => 'homes#about'
+    resources :items, only: [:index, :show]
+    resource :customers, only: [:show, :edit, :update] do
+      get '/confirm' => 'customers#confirm'
+      patch '/deleted' => 'customers#deleted'
+    end
+
+    resources :cart_items, only: [:index, :update, :create, :destroy]
+    delete 'cart_items/destroy_all' => 'cart_items#destroy_all'
+
+    resources :orders, only: [:new, :create, :index, :show]
+    post 'orders/confirm' => 'orders#confirm'
+    get 'orders/complete' => 'orders#complete'
+    resources :addresses, except: [:new, :show]
+  end
+
+
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
