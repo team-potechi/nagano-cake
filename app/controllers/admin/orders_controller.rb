1084fed4c3eaeb26465aa611706eef21e0b74b54
kotatsuko <107920100+kotatsuko@users.noreply.github.com>
2022-08-17 15:46:20 +0000

    [add]order,order_detailのコントローラー内のアクション記述とモデル記述,[start]orderのshowビューの記述

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,11 +1,25 @@
 class Admin::OrdersController < ApplicationController
-  
+
+  layout "admin_application"
+
   def show
-    
+    @order = Order.find(params[:id])
   end
-  
+
   def update
-    
+    @order = Order.find(params[:id])
+    if @order.update(order_params)
+      redirect_to order_path(@order), notice: "注文ステータスを変更しました"
+    else
+      render "show", notice: "注文ステータスの変更が保存されませんでした"
+    end
   end
-  
+
+
+
+  private
+
+  def order_params
+    params.require(:order).permit(:status)
+  end
 end
