4addf0a2303e7b4fdcde6268a20e71fb2ed0b210
kotatsuko <107920100+kotatsuko@users.noreply.github.com>
2022-08-17 08:02:42 +0000

    [fix]トップ画面の新着商品の表示数修正

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -3,7 +3,7 @@ class Public::HomesController < ApplicationController
   layout "public_application"
 
   def top
-    @new_items = Item.all.order("created_at DESC").first(3)
+    @new_items = Item.all.order("created_at DESC").first(4)
   end
 
   def about
