3fdfdf876be07009a46e62a37026071cce6aebf0
kitten1139 <106716550+kitten1139@users.noreply.github.com>
2022-08-18 08:21:35 +0000

    顧客の退会確認画面と退会処理(ステータスの更新)機能の追加

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -3,12 +3,22 @@ class Public::CustomersController < ApplicationController
   def show
     @customer = current_customer
   end
-  
-  
+
+  def confirm
+    @customer = current_customer
+  end
+
+  def deleted
+    @customer = current_customer
+    @customer.update(is_deleted: true)
+    flash[:notice] = "退会処理を実行いたしました"
+    reset_session
+    redirect_to root_path
+  end
 
   private
 
   def customer_params
-    params.require(:customer).permit(:lastname, :firstname, :kana_lastname, :kana_firstname, :postal_code, :address, :tel, :email)
+    params.require(:customer).permit(:lastname, :firstname, :kana_lastname, :kana_firstname, :postal_code, :address, :tel, :email, :password)
   end
 end
