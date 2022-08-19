48c4220a0663d19356aa84208a36d45e81686d1d
Your Name <107790440+daikingkong@users.noreply.github.com>
2022-08-19 10:16:06 +0000

    [Add]商品の新規登録機能と登録画面の追加

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,2 +1,41 @@
 class Admin::ItemsController < ApplicationController
+
+  layout 'admin_application'
+
+  def new
+    @item = Item.new
+    @genres = Genre.all
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      flash[:notice] = "商品を登録しました"
+      redirect_to admin_item_path(@item)
+    else
+      flash[:notice] = "商品の登録ができませんでした"
+      render :new
+    end
+  end
+
+  def index
+    @item = Item.find(params[:id])
+    @genres = Genre.all
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :genre_id, :introduction, :price, :sale_status, :item_image)
+  end
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,11 @@
 class Item < ApplicationRecord
+  has_many :cart_items
+  has_many :order_details
+  belongs_to :genre
+
+  has_one_attached :item_image
+
+  def get_item_image
+    (item_image.attached?) ? item_image : 'no_image.jpg'
+  end
 end
