531b2ac34e544a87b37b210a78d95d1f00fa9812
kitten1139 <106716550+kitten1139@users.noreply.github.com>
2022-08-19 10:10:19 +0000

    [Add]顧客情報の更新機能作成

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -13,9 +13,15 @@ class Admin::CustomersController < ApplicationController
     @customer = Customer.find(params[:id])
   end
 
+  def update
+    @customer = Customer.find(params[:id])
+    @customer.update(customer_params)
+    redirect_to admin_customer_path(@customer)
+  end
+
   private
 
   def customer_params
-    params.require(:customer).permit(:lastname, :firstname, :kana_lastname, :kana_firstname, :postal_code, :address, :tel, :email, :password)
+    params.require(:customer).permit(:lastname, :firstname, :kana_lastname, :kana_firstname, :postal_code, :address, :tel, :email, :password, :is_deleted)
   end
 end
