bb4f7066c35e61abaa3e810c4aaf6ab8f2c5057b
Your Name <107790440+daikingkong@users.noreply.github.com>
2022-08-20 09:14:07 +0000

    [Add]顧客の商品一覧画面の追加

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,2 +1,14 @@
 class Public::ItemsController < ApplicationController
+
+  layout "public_application"
+
+  def index
+    @items = Item.page(params[:page]).per(8)
+    @genres = Genre.all
+  end
+
+  def show
+    @item = Item.find(params[:id])
+  end
+
 end
