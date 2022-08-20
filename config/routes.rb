d377d8c9fd2859deb001ef8d686bdf3852a493f5
kotatsuko <107920100+kotatsuko@users.noreply.github.com>
2022-08-20 09:50:14 +0000

    [add]注文情報入力画面(支払方法・配送先の選択),注文情報確認画面,注文完了画面,注文確定処理

--- config/routes.rb
+++ config/routes.rb
@@ -30,9 +30,10 @@ Rails.application.routes.draw do
     delete 'cart_items/destroy_all' => 'cart_items#destroy_all'
     resources :cart_items, only: [:index, :update, :create, :destroy]
 
-    resources :orders, only: [:new, :create, :index, :show]
     post 'orders/confirm' => 'orders#confirm'
     get 'orders/complete' => 'orders#complete'
+    resources :orders, only: [:new, :create, :index, :show]
+
     resources :addresses, except: [:new, :show]
   end
 
