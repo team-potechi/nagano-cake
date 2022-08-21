563b66a3f737e73718333e33772cec0fafb2237e
kitten1139 <106716550+kitten1139@users.noreply.github.com>
2022-08-21 00:44:09 +0000

    [Add]customerモデルにバリデーション追加

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -8,5 +8,13 @@ class Customer < ApplicationRecord
   has_many :orders, dependent: :destroy
   has_many :cart_items, dependent: :destroy
 
+  validates :lastname, presence: true
+  validates :firstname, presence: true
+  validates :kana_lastname, presence: true
+  validates :kana_firstname, presence: true
+  validates :postal_code, presence: true
+  validates :address, presence: true
+  validates :tel, presence: true
+  validates :email, presence: true
 
 end
