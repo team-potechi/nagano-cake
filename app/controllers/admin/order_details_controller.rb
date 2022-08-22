e356a16d34482a1ddcf1fb853e96aa2a60e70fd7
kotatsuko <107920100+kotatsuko@users.noreply.github.com>
2022-08-22 07:11:21 +0000

    [fix]注文ステータスと製作ステータスの連動

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -5,7 +5,14 @@ class Admin::OrderDetailsController < ApplicationController
 
   def update
     @order_detail = OrderDetail.find(params[:id])
+    @order = @order_detail.order
     if @order_detail.update(order_detail_params)
+      if @order.order_details.any?{|order_detail| order_detail.making_status == "製作中"}
+        @order.update(status: "製作中")
+      end
+      if @order.order_details.all?{|order_detail| order_detail.making_status == "製作完了"}
+        @order.update(status: "発送準備中")
+      end
       redirect_to admin_order_path(@order_detail.order), notice: "製作ステータスを変更しました"
     else
       render admin_order_path, notice: "製作ステータスの変更が保存されませんでした"
--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -9,11 +9,18 @@ class Admin::OrdersController < ApplicationController
   def show
     @order = Order.find(params[:id])
     @order_details = @order.order_details
+
   end
 
   def update
     @order = Order.find(params[:id])
+    @order_details = @order.order_details
     if @order.update(order_params)
+      if @order.status == "入金待ち"
+        @order_details.update_all(making_status: "着手不可")
+      elsif @order.status == "入金確認"
+        @order_details.update_all(making_status: "製作待ち")
+      end
       redirect_to admin_order_path(@order), notice: "注文ステータスを変更しました"
     else
       render "show", notice: "注文ステータスの変更が保存されませんでした"
