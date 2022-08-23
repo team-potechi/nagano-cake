a8a92d0ddf82e657615903e0f72b09ac5473d92a
kitten1139 <106716550+kitten1139@users.noreply.github.com>
2022-08-23 06:47:11 +0000

    [Fix]管理者側のヘッダー修正

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -2,7 +2,7 @@
 
 class Admin::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
-  layout "public_application"
+  layout "admin_application"
 
   protected
   def after_sign_in_path_for(resource)
